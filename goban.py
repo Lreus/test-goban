05c9e921f790ecc5d8d1679c8335eaa5c4f4bfc9
Lreus <ludovic.reus@free.fr>
2022-03-08 21:59:58 +0100

    finaly I found a solution

--- goban.py
+++ goban.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 import enum
 
 
@@ -42,5 +43,38 @@ class Goban(object):
         elif self.goban[y][x] == "#":
             return Status.BLACK
 
-    def is_taken(self, x, y):
-        pass
+        raise RuntimeError(f'Unexpected char in goban table {self.goban[y][x]}')
+
+    def is_taken(self, x: int, y: int) -> bool:
+        starting_status = self.get_status(x, y)
+
+        if starting_status == Status.EMPTY or self.goban_has_one_slot():
+            return False
+
+        to_test = [(x, y)]
+        already_tested = []
+
+        while len(to_test) > 0:
+            current_position = to_test.pop()
+            for comparison in self.get_position_around(*current_position):
+                if comparison not in already_tested:
+                    compared_status = self.get_status(*comparison)
+                    if compared_status == Status.EMPTY:
+                        return False
+                    if compared_status == starting_status:
+                        to_test.append(comparison)
+            already_tested.append(current_position)
+
+        return True
+
+    def get_position_around(self, a: int, b: int) -> List[Tuple]:
+        right = (a + 1, b)
+        down = (a, b + 1)
+        left = (a - 1, b)
+        up = (a, b - 1)
+
+        return [right, down, left, up]
+
+    def goban_has_one_slot(self) -> bool:
+        return len("".join(self.goban)) == 1
+
--- test_goban.py
+++ test_goban.py
@@ -1,61 +1,105 @@
+import unittest
+
 from goban import Goban
 
 
-def test_white_is_taken_when_surrounded_by_black():
-    goban = Goban([
-        '.#.',
-        '#o#',
-        '.#.',
-    ])
+class GobanTest(unittest.TestCase):
+    def test_empty_slot_cannot_be_taken(self):
+        goban = Goban(['o'])
 
-    assert goban.is_taken(1, 1) is True
+        self.assertFalse(goban.is_taken(0, 0))
 
+    def test_alone_piece_cannot_be_taken(self):
+        goban = Goban(['#.'])
 
-def test_white_is_not_taken_when_it_has_a_liberty():
-    goban = Goban([
-        '...',
-        '#o#',
-        '.#.',
-    ])
+        self.assertFalse(goban.is_taken(0, 0))
 
-    assert goban.is_taken(1, 1) is False
+    def test_two_different_piece_can_be_taken(self):
+        goban = Goban(['#o'])
 
+        self.assertTrue(goban.is_taken(0, 0))
 
-def test_black_shape_is_taken_when_surrounded():
-    goban = Goban([
-        'oo.',
-        '##o',
-        'o#o',
-        '.o.',
-    ])
+    def test_liberty_down_cannot_be_taken(self):
+        goban = Goban([
+            '#o',
+            '..'
+        ])
 
-    assert goban.is_taken(0, 1) is True
-    assert goban.is_taken(1, 1) is True
-    assert goban.is_taken(1, 2) is True
+        self.assertFalse(goban.is_taken(0, 0))
 
+    def test_cornered_piece_can_be_taken(self):
+        goban = Goban([
+            '#o',
+            'o.'
+        ])
 
-def test_black_shape_is_not_taken_when_it_has_a_liberty():
-    goban = Goban([
-        'oo.',
-        '##.',
-        'o#o',
-        '.o.',
-    ])
+        self.assertTrue(goban.is_taken(0, 0))
 
-    assert goban.is_taken(0, 1) is False
-    assert goban.is_taken(1, 1) is False
-    assert goban.is_taken(1, 2) is False
+    def test_left_liberty_cannot_be_taken(self):
+        goban = Goban([
+            '.#o',
+            '.o.'
+        ])
 
+        self.assertFalse(goban.is_taken(1, 0))
 
-def test_square_shape_is_taken():
-    goban = Goban([
-        'oo.',
-        '##o',
-        '##o',
-        'oo.',
-    ])
+    def test_up_piece_can_be_taken(self):
+        goban = Goban([
+            'o#o',
+            '.o.'
+        ])
 
-    assert goban.is_taken(0, 1) is True
-    assert goban.is_taken(0, 2) is True
-    assert goban.is_taken(1, 1) is True
-    assert goban.is_taken(1, 2) is True
+        self.assertTrue(goban.is_taken(1, 0))
+
+    def test_up_liberty_cannot_be_taken(self):
+        goban = Goban([
+            '.o.',
+            'o#o',
+            '.o.'
+        ])
+
+        self.assertTrue(goban.is_taken(1, 1))
+
+    def test_rounded_line_can_be_taken(self):
+        goban = Goban([
+            '.oo.',
+            'o##o',
+            '.oo.'
+        ])
+
+        self.assertTrue(goban.is_taken(1, 1))
+        self.assertTrue(goban.is_taken(2, 1))
+
+    def test_open_rounded_line_can_not_be_taken(self):
+        goban = Goban([
+            '.oo.',
+            'o##.',
+            '.oo.'
+        ])
+
+        self.assertFalse(goban.is_taken(1, 1))
+        self.assertFalse(goban.is_taken(2, 1))
+
+    def test_l_shape_can_be_taken(self):
+        goban = Goban([
+            '.oo.',
+            'o#o.',
+            'o#oo',
+            'o###o',
+            '.ooo'
+        ])
+
+        self.assertTrue(goban.is_taken(2, 3))
+
+    def test_square_shape_can_be_taken(self):
+        goban = Goban([
+            'oo.',
+            '##o',
+            '##o',
+            'oo.',
+        ])
+
+        self.assertTrue(goban.is_taken(0, 1))
+        self.assertTrue(goban.is_taken(0, 2))
+        self.assertTrue(goban.is_taken(1, 1))
+        self.assertTrue(goban.is_taken(1, 2))
